94214813c1667b4c98eac5faca40182b01267d3c
Matt Behrens <askedrelic@gmail.com>
2010-03-02 16:33:01 -0500

    Cleanup

--- RUNME.php
+++ RUNME.php
@@ -1,13 +1,19 @@
 <?php
+//Author: Matt Behrens <askedrelic@gmail.com>
+//PHP library to post birthdays, daily info, and awards on shacknews.com forums.
+
 require 'postbot.php';
 
+//Create new Postbot with your Shacknews username and password
 $a = new PostBot('USERNAME','PASSWORD');
 
+//Add whichever posts you want
 $a->addPost(new BirthdayPost());
 $a->addPost(new LolPost());
 $a->addPost(new TagPost());
 $a->addPost(new UnfPost());
 $a->addPost(new InfPost());
 
+//Post them!
 $a->makePosts();
 ?>
