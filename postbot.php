c2a3048ec1a14fbebcc6d25fa4e4a1114919288a
Matt Behrens <askedrelic@gmail.com>
2012-11-05 19:48:35 -0500

    adding this date diff code

--- postbot.php
+++ postbot.php
@@ -14,6 +14,16 @@ require_once 'awardPost.php';
 
 //set timezone to work with DateTime objects
 date_default_timezone_set('America/New_York');
+function date_diff($date1, $date2) { 
+    $current = $date1; 
+    $datetime2 = date_create($date2); 
+    $count = 0; 
+    while(date_create($current) < $datetime2){ 
+        $current = gmdate("Y-m-d", strtotime("+1 day", strtotime($current))); 
+        $count++; 
+    } 
+    return $count; 
+} 
 
 class PostBot {
     public $username;
@@ -45,12 +55,14 @@ class PostBot {
 
         print $body;
 
-        $body .= system("curl -Is slashdot.org | egrep '^X-(F|B|L)' | sed s/^X-//");
+        //$body .= system("curl -Is slashdot.org | egrep '^X-(F|B|L)' | sed s/^X-//");
 
         //TODO create quote database to use here
-        $body .= "\n\n";
+        //$body .= "\n\n";
         // $body .= "This is the Best Of shacknews:";
-        $body .= $this->insertShackconRelease();
+        //$body .= $this->insertShackconRelease();
+        
+        $body .= $this->insertBrownies();
 
         $p->body = $body;
         //make first post and override parentId
@@ -96,13 +108,47 @@ class PostBot {
         }
     }
 
+    private function insertBrownies() {
+        $today = new DateTime("now");
+        $day = $today->format("Y-m-d");                                                                                                                                                                                                                                                 
+
+        $ret = "\n";
+        $ret .= "HEY p[MULTISYNC]p buy some delicious ";
+
+        if ($day == "2012-10-17") {
+            $ret .= "Today only, r{Chocolate Overload Espresso Brownies}r";
+        } elseif ($day == "2012-10-18") {
+            $ret .= "Today only, r{Salted Caramel Chocolate Cookies}r";
+        } elseif ($day == "2012-10-19") {
+            $ret .= "r{Kiss of Chocolate Cherry Cookies}r";
+        } elseif ($day == "2012-10-22") {
+            $ret .= "r{Chewy Lemon Snowball Cookies}r";
+        } elseif ($day == "2012-10-23") {
+            $ret .= "r{Butterfinger Blast Crunchy Cookies}r";
+        } elseif ($day == "2012-10-24") {
+            $ret .= "r{Pumpkin Chocolate Chip Cake Bars}r";
+        } elseif ($day == "2012-10-25") {
+            $ret .= "r{Apple Cider Caramel Cookies}r";
+        } elseif ($day == "2012-10-30") {
+            $ret .= "r{Puff Power Cinnamon Snickerdoodles}r";
+        } elseif ($day == "2012-10-31") {
+            $ret .= "r{Banana Bread Chocolate Chip Cookies}r";
+        } elseif ($day == "2012-11-01") {
+            $ret .= "r{Cinnamon Hazelnut Biscotti}r";
+        } else {
+            return "";
+        }
+        $ret .= ", today only at http://www.defconyum.com/\n";
+        return $ret;
+    }
+
     private function insertShackconRelease() {
         $launch_date = new DateTime('2012-07-08');
         $today = new DateTime("now");
-        $interval = $launch_date->diff($today);
+        $interval = date_diff($launch_date, $today);
         if ($interval->d > 1) {
             return "There are ". $interval->d ." days until Shackcon 2012!\n";
-        if ($interval->d == 1) {
+        } elseif ($interval->d == 1) {
             return "There is ". $interval->d ." day until Shackcon 2012!\n";
         } elseif ($interval->d == 0) {
             return "ZOMG VEGAS SHACKCON!!!\n";
