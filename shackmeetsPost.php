f79ed8c14e3ad6eba4a4513cec5d415eae11d067
omnova <omnova@gmail.com>
2014-05-23 00:53:54 -0500

    Added ShackmeetsPost class
    Added ShackmeetsPost class to create the upcoming shackmeets post.

---
+++ shackmeetsPost.php
@@ -0,0 +1,18 @@
+<?php
+
+// Posts upcoming shackmeets information
+class ShackmeetsPost extends Post 
+{
+  public function __construct()
+  {
+    $url = 'http://www.shackmeets.com/api/shackmeetspost';
+    $shackmeetsText = parent::curlData($url);
+
+    $body = "_[p[Upcoming Shackmeets:]p]_ \n\n";
+    $body .= strlen($shackmeetsText) > 0 ? $shackmeetsText : 'None. :(';
+      
+    parent::__construct($body);
+  }
+}
+
+?>
