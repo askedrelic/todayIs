61cc0b79320d446d223697b4d26390120d02bd30
Matt Behrens <askedrelic@gmail.com>
2015-10-12 20:34:49 +0000

    sane defaults

--- SAMPLE.php
+++ SAMPLE.php
@@ -3,6 +3,7 @@
 //PHP library to post birthdays, daily info, and awards on shacknews.com forums.
 
 require 'postbot.php';
+set_time_limit(3600);
 
 //Create new Postbot with your Shacknews username and password
 $a = new PostBot('USERNAME','PASSWORD');
@@ -11,8 +12,8 @@ $a = new PostBot('USERNAME','PASSWORD');
 $a->addPost(new BirthdayPost());
 $a->addPost(new LolPost());
 $a->addPost(new TagPost());
-$a->addPost(new UnfPost());
 $a->addPost(new InfPost());
+$a->addPost(new ShackmeetsPost());
 
 //Post them!
 $a->makePosts();
